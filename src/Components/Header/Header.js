495fe64d93ba8b9d51f49c8ee5586fe62fbd3c1e
DevCatsi <jus.sobcz@gmail.com>
2020-03-30 16:22:26 +0200

    Add basic searching feature.

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,13 +2,45 @@ import React, {Component} from 'react'
 import "./Header.css"
 import logoImage from "../../assets/cat-logo.svg"
 import logoText from "../../assets/love-cat.svg"
-
+import axios from "axios"
+const API_KEY = '15810825-92ac7d5556d19337c67cc6d1a';
 class Header extends Component {
 state = {
+    catImages: [],
     value: "",
+    searching: false,
+};
+    handleChange = (e)=>{
+        let value = e.target.value;
+        this.setState({
+            value
+        })
+
+    };
+handleClick = (e)=>{
+    console.log('działa: ', e);
+
+    if(this.state.value.length !==0){
+        const url = `https://pixabay.com/api/?key=${API_KEY}&q=${this.state.value}&image_type=photo`;
+        console.log(url);
+        axios.get(url)
+            .then(response=>{
+                //data.hits[""0""].webformatURL
+                console.log(response.data.hits)
+                const catImages = response.data.hits.map(hit=>hit.webformatURL)
+                console.log(catImages);
+                this.setState({
+                    catImages,
+                    searching: false,
+                })
+            });
+        this.setState({
+        searching: true,
+        })
+    }
+
 };
     render() {
-
         return (
             <React.Fragment>
                 <div className='container'>
@@ -25,8 +57,8 @@ state = {
                     <h2 className="title">cats photo gallery</h2>
                     <p className='sentence'><span>"Najmarniejszy kot jest arcydziełem."</span> - Leonardo da Vinci</p>
                     <div className='searchBar'>
-                        <input type="text" placeholder="wpisz frazę..." className='searcher'/>
-                        <button className='btnstyle'>Znajdź</button>
+                        <input type="text" placeholder="wpisz frazę..." className='searcher' onChange={this.handleChange}/>
+                        <button className='btnstyle' onClick={this.handleClick}>Znajdź</button>
                     </div>
                     </div>
                 </div>
